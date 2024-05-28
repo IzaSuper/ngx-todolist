222a1cbcf002f53a86162c39280b879c571eeac0
IzaSuper <izabelasuperczynska@gmail.com>
2024-05-28 12:34:42 +0200

    Add filter pipes.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,17 +1,27 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {CommonModule, NgForOf} from "@angular/common";
 import {Item} from "../interface/item";
+import {FormsModule} from "@angular/forms";
+import {filterTitlePipe} from "../../pipes/filter-title.pipe";
+import {filterDescriptionPipe} from "../../pipes/filter-description.pipe";
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    FormsModule,
+    CommonModule,
+    filterTitlePipe,
+    filterDescriptionPipe
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  filterByTitle= ''
+  filterByDescription = ''
   @Input() data: Item[] = []
   @Output() deleted = new EventEmitter<number>()
 
